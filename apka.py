d73fc4ea46eb3ca85cba44cc41124fe09fafee0b
Patryk <patryk.nowotka@gmail.com>
2023-09-21 18:34:21 +0000

    Adding apka.py

---
+++ apka.py
@@ -0,0 +1,13 @@
+import json
+import os
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.dropdown import DropDown
+from kivy.uix.popup import Popup
+from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
